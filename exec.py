98dbdcdbc5118b3a6a9d83223eb84b0c623787d0
M1-Elmasry <mostafamfelmasry@outlook.com>
2023-05-09 23:58:28 +0300

    fix executing

--- exec.py
+++ exec.py
@@ -1,16 +1,18 @@
 import os
 
-def execute(alias_name: str, cur: object) -> int:
+def execute(argv: list, cur: object) -> int:
 
     browser = os.getenv('BROWSER')
     data_tuple = cur.execute("SELECT * FROM data")
 
     for row in data_tuple:
-        if (row[0] == alias_name):
+        if (row[0] == argv[1]): # argv[1] = alias_name
             for command in row[1].split("?:?"):
                 if (browser in command):
                     if (input("you want to close existing browser sessions y or n: ") == "y"):
                         os.system(f"killall -9 {browser}")
-                os.system(command)
+                os.system(f"{command} {' '.join(argv[2:])}") # to execute any command after alias name
+                # say the user type "ze add <file_name>" and "add" is equal "git add"
+                # in this case ze must execute all line "ze "git add" <file_name>"
             return (0)
     return (-1)
